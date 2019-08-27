38959c5e69e35127440dc1eefb32720023373859
adminshabbir <ahamadshabbir2@gmail.com>
2019-08-27 07:51:08 -0400

    added repository details

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -14,9 +14,9 @@ job('NodeJS Docker example') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('wardviaene/docker-nodejs-demo')
+            repositoryName('adminshabbir/docker-nodejs-demo1')
             tag('${GIT_REVISION,length=9}')
-            registryCredentials('dockerhub')
+            registryCredentials('adminshabbir')
             forcePull(false)
             forceTag(false)
             createFingerprints(false)
